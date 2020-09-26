fa811a50e87f7490e2a9d5bf48c28dfe6c200532
warleyt <45487303+warleyt@users.noreply.github.com>
2020-09-26 16:31:21 -0300

    Add files via upload

---
+++ test/teste/teste.java
@@ -0,0 +1,13 @@
+package teste;
+
+public class teste {
+    public static void main(String[] args) {
+        double a=Math.floor(4/3);
+        int tam=1;
+        System.out.println(a);
+        System.out.println(tam);
+        for (int i = 0; i < tam/3; i++) {
+            System.out.println(i+1);
+        }
+    }
+}
